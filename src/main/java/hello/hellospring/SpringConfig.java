ce28e00000afd288fdc509ef4d96390d1bb5041c
pci <aswq233333@naver.com>
2023-04-05 14:37:28 +0900

    2023.04.05

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,14 +1,25 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 // 자바 코드로 직접 스프링 빈 등록하기
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -17,6 +28,10 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        // 객체지향의 장점은 다형성을 활용하여 인터페이스를 두고
+        // 구현체를 바꿔서 사용할 수 있음
+        // 스프링은 이것을 편리하게 사용할 수 있도록 스프링 컨테이너가 지원을 해줌
+        return new JdbcMemberRepository(dataSource);
     }
 }
